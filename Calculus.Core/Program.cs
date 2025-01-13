f257096fffa523f7249be24633d51631f5b7c6bb
Henri Perruchon <hperruchon@gmail.com>
2025-01-14 00:01:35 +0100

    Feats : - Add .sln file - Add Calculus.Core project

---
+++ Calculus.Core/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey(); // Keeps the console open until a key is pressed
